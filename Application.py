5b46d1e77b6be7f33736ee156914dbc5aa0d612b
OziomaEunice <onwubikooziomaeunice@gmail.com>
2023-02-27 15:34:52 +0000

    Flask environment set up

--- Application.py
+++ Application.py
@@ -10,7 +10,7 @@ from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 
 # home page
-# @app.route('/')
-# @app.route('/home/')
-# def home():
-#     return render_template('home.html')
+@app.route('/')
+@app.route('/home/')
+def home():
+    return render_template('home.html')
