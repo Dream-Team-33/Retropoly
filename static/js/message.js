2707b551a45934eee20dc16daeaf1ac61f3641e9
Ryan-SD <ryanmccormick02@outlook.com>
2023-03-08 13:59:30 +0000

    Message.js and dashboard.html updated message.js -changed from using jquery to standard js dashboard.html -Changed the function calls to ensure that they are valid for new message.js file

--- static/js/message.js
+++ static/js/message.js
@@ -1,13 +1,33 @@
 function openPopup(popup) {
 	/* Open popup and make accessible screen readers */
+
+
+	//Looks for an id with the value of what is passed by the function call on the dashboard.html file
+	var specifiedPopup = document.getElementById(popup) 
+	//assigns the display style "none" to hide the popup from the user
+	specifiedPopup.style.display = "block"
+
+
+	/* IGNORE Requires JQUERY AND WE DONT HAVE IT INTSALLED CURRENTLY
 	$(popup).show().attr("aria-hidden", "false");
-	/* Focus on element to guide screen readers to popup */
-	$("#closePopup").focus();
+	Focus on element to guide screen readers to popup
+	$("#closePopup").focus();*/
 }
 
 function closePopup(popup) {
 	/* Close popup and hide from screen readers */
+
+
+	//Looks for an id with the value of what is passed by the function call on the dashboard.html file
+	var specifiedPopup = document.getElementById(popup)
+	//assigns the display style "none" to hide the popup from the user
+	specifiedPopup.style.display = "none"
+
+
+	/*
+	///IGNORE Requires JQUERY AND WE DONT HAVE IT INTSALLED CURRENTLY
 	$(popup).hide().attr("aria-hidden", "true");
-	/* Focus screen readers back to button */
+	Focus screen readers back to button 
 	$("#openMyPopup").focus();
+	*/
 }
