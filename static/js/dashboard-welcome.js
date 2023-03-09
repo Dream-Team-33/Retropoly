9fc0fcd2d9011bc19b5105844bc7c0fa4d6eb1de
OziomaEunice <onwubikooziomaeunice@gmail.com>
2023-03-09 17:31:04 +0000

    preloader added

--- static/js/dashboard-welcome.js
+++ static/js/dashboard-welcome.js
@@ -16,3 +16,22 @@ var typed = new Typed(".auto-type", {
             backSpeed: 90,
             loop: true
 })
+
+
+
+
+/*This is for the preloader*/
+//define the loader variable
+var loader = document.getElementById("preloader");
+
+//create an event function
+window.addEventListener("load", function(){
+    //hide the loader
+    loader.style.display = "none";
+
+    /**The loader works, however, it disappears very quickly
+     * when refreshing it, so what can be done is to have 
+     * a video link (see profile.html for tag iframe) and
+     * paste it multiple times
+     */
+})
