1ae26546dad05ed3916f49d776c261d5b4b12344
snadyfr <98316473+snadyfr@users.noreply.github.com>
2023-03-13 17:16:44 +0000

    Added SocketIO connection and debuggin

--- Application.py
+++ Application.py
@@ -6,8 +6,29 @@
 from fileinput import filename
 from os import abort
 from flask import Flask, redirect, url_for, request, render_template
+from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on_error()        # Handles the default namespace
+def error_handler(e):
+    pass
+
+@socketio.on_error_default  # handles all namespaces without an explicit error handler
+def default_error_handler(e):
+    pass
+
 
 # home page
 @app.route('/')
@@ -26,14 +47,24 @@ def dashboardWelcome():
     return render_template('dashboard-welcome.html')
 
 # dashboard page
-@app.route('/dashboard/')
+@app.route('/dashboard/', methods=['GET','POST'])
 def dashboard():
-    return render_template('dashboard.html')
+    if request.method == 'POST':
+        hours_for_selected = request.form['hours']
+        return render_template('dashboard.html', hours=hours_for_selected)
+    else:
+        
+        return render_template('dashboard.html')
 
 # notes page
-@app.route('/dashboard/notes')
+@app.route('/dashboard/notes', methods=['GET','POST'])
 def notes():
-    return render_template('Notes.html')
+    if request.method == 'POST':
+        notes_title = request.form['notes_title']
+        notes_content = request.form['notes_content']
+        return render_template('Notes.html', notes=notes_content, title=notes_title)
+    else:  
+        return render_template('Notes.html')
 
 # rules page
 @app.route('/dashboard/rules')
@@ -44,3 +75,6 @@ def rules():
 @app.route('/dashboard/teams')
 def teams():
     return render_template('Teams.html')
+
+if __name__ == '__main__':
+    socketio.run(app)
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,24 +1,4 @@
-//Selects task
-//get all tasks
-var tasks = document.querySelectorAll(".task");
-
-//wait for any clicks
-tasks.forEach(function(task) {
-  task.addEventListener("click", function() {
-    //when clicked take out all other options
-    tasks.forEach(function(t) {
-      t.classList.remove("selected");
+var socket = io();
+    socket.on('connect', function() {
+        socket.emit('my event', {data: 'I\'m connected!'});
     });
-    task.classList.add("selected");
-  });
-});
-
-
-document.getElementById("send-button").addEventListener("click", function() {
-  var selectedTask = document.querySelector(".task.selected");
-  var selectedTaskId = selectedTask.id;
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/my-flask-endpoint');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify({ selectedTaskId: selectedTaskId }));
-});
