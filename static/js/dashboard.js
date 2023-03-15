06304acdb2b628bed96319835288625ebbe943bc
snadyfr <98316473+snadyfr@users.noreply.github.com>
2023-03-15 18:11:08 +0000

    fixed the issues

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -21,7 +21,6 @@ function selectTask(taskElement) {
 
 
     if (selectedTask !== null) {
-<<<<<<< HEAD
     // Unselect the previously selected task
     selectedTask.classList.remove("selected-task");
     }
@@ -74,36 +73,3 @@ function updateHoursRemaining(taskElement, newHoursText) {
     hoursRemainingElement.textContent = (newHoursText.toString() + "hrs");
     taskElement.classList.remove("selected-task");
 }
-=======
-        // Unselect the previously selected task
-        selectedTask.classList.remove("selected-task");
-    }
-    
-    var parent = selectedTask.parentNode;
-    console.log(parent)
-    // rest of your code that uses parent
-
-    // checks to see if the classes within the parent node of the slected task contains either 'backlog' or 'done'
-    if (parent.classList.contains('backlog') || parent.classList.contains('done')) {
-        //
-        // popup should appear explaining why they cannot select the card that they have selected
-        //
-        //
-        
-        // debuggign console log to tell us that they card is not a valid choice
-        console.log("backlog or done detected please choose a valid card!")
-    }
-    else
-    {
-        // Select the new task
-        // adds selected-task class to the class list of the parent node of the button clicked to select the card
-        selectedTask.classList.add("selected-task");
-    }
-
-    //TODO: disallow the user to reroll on something that is selected and is within the done section or backlog(should be impossible to go back to backlog)
-
-    
-
-    
-}
->>>>>>> b266defa5104acf26a25e8b64475532c04d9be04
