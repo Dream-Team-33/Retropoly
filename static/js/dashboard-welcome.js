0311ecd704c1bf7d6c2b9beb29262555ae824354
OziomaEunice <onwubikooziomaeunice@gmail.com>
2023-03-30 15:31:59 +0100

    animated-text in dashboard-welcome page updated

--- static/js/dashboard-welcome.js
+++ static/js/dashboard-welcome.js
@@ -7,10 +7,11 @@
 var typed = new Typed(".auto-type", {
 	strings: [
 		"Start",
+		"&#128518;",
 		"Click on 'Dashboard' to start the game",
-		"Or click on the 'Rules' tab to understand the rules of the game.",
-		"'Teams' tab is to view your team...",
-		"...while 'Notes' is cliché",
+		"You will find 'Rules' to understand the rules of the game...",
+		"...while 'Notes' is cliché.",
+		"Have fun!&#128540;",
 	],
 	//Speed of the text will be 50 when typing,
 	//and 10 when cancelling out.
